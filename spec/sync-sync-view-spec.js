612182076b8605ab6131e3daed1dfeefba368b79
Tomohiro Murota <tomohiro.murota@gmail.com>
2018-01-06 15:51:54 +0900

    Add package templates

---
+++ spec/sync-sync-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SyncSyncView from '../lib/sync-sync-view';
+
+describe('SyncSyncView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
