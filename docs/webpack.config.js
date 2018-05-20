57ca705436c27592b0675eaec23f8a4e41edf418
Tomohiro Murota <tomohiro.murota@gmail.com>
2018-05-20 22:24:35 +0900

    Add project site style base

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: {
     'index': [
-    path.resolve(__dirname, 'src/index.js'),
+      path.resolve(__dirname, 'src/index.js'),
     ],
   },
   output: {
@@ -19,10 +19,26 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              attrs: {
+                nonce: 'fUz9xyElzZh7x4XwbKkt2OdAhPs='
+              },
+            },
+          },
+          'css-loader',
+          'less-loader',
+        ],
+      },
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.less'],
   },
 }
 
